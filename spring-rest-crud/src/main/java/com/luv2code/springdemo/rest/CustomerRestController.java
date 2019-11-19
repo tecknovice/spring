1cdc31559f7d9dbfbf82386c720398726b453b96
tecknovice <tecknovice@gmail.com>
2019-11-19 15:10:02 +0700

    add spring security to spring-rest-crud project

--- spring-rest-crud/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-rest-crud/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -19,41 +19,74 @@ import com.luv2code.springdemo.service.CustomerService;
 @RequestMapping("/api")
 public class CustomerRestController {
 
+	// autowire the CustomerService
 	@Autowired
 	private CustomerService customerService;
 	
+	// add mapping for GET /customers
 	@GetMapping("/customers")
-	public List<Customer> getCustomers(){
+	public List<Customer> getCustomers() {
+		
 		return customerService.getCustomers();
+		
 	}
 	
-	@GetMapping("/customers/{id}")
-	public Customer getCustomer(@PathVariable int id) {
-		Customer customer = customerService.getCustomer(id);
-		if(customer==null)
-			throw new CustomerNotFoundException("Customer not found: "+id);
-		return customer;
+	// add mapping for GET /customers/{customerId}
+	
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		
+		return theCustomer;
 	}
+	
+	// add mapping for POST /customers  - add new customer
 	
 	@PostMapping("/customers")
-	public Customer create(@RequestBody Customer customer) {
-		customer.setId(0);
-		customerService.saveCustomer(customer);
-		return customer;
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		// also just in case the pass an id in JSON ... set id to 0
+		// this is force a save of new item ... instead of update
+		
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
 	}
+	
+	// add mapping for PUT /customers - update existing customer
 	
 	@PutMapping("/customers")
-	public Customer update(@RequestBody Customer customer) {
-		customerService.saveCustomer(customer);
-		return customer;
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+		
 	}
 	
-	@DeleteMapping("/customers/{id}")
-	public String delete(@PathVariable int id) {
-		Customer customer = customerService.getCustomer(id);
-		if(customer==null)
-			throw new CustomerNotFoundException("Customer not found: "+ id);
-		customerService.deleteCustomer(id);
-		return "Deleted Successfully!";
+	// add mapping for DELETE /customers/{customerId} - delete customer
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		
+		// throw exception if null
+		
+		if (tempCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+				
+		customerService.deleteCustomer(customerId);
+		
+		return "Deleted customer id - " + customerId;
 	}
+	
 }
